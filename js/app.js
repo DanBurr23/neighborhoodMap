155c98eb3e85cb15220fc2c7bffcd5e06e922d5e
DanBurr23 <danburrus@sbcglobal.net>
2017-12-30 15:31:36 -0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,144 @@
+'use strict';
+
+//make the map a global variable
+var map;
+
+// Set up the ViewModel
+var ViewModel = function() {
+    
+  var self = this;
+  
+  this.lunchSpotList = ko.observableArray([]);
+  
+  // Search term is blank by default
+  this.searchTerm = ko.observable('');
+
+  // load initial map
+  map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 37.766555, lng: -121.963462},
+      zoom: 14
+  });
+
+	//create a marker to indicate where they currently are on the map
+	var marker = new google.maps.Marker({
+		position: {lat: 37.766555, lng: -121.963462},
+		title:"You are here!",
+		icon: "images/att logo.jpg"
+	});
+	
+	marker.setMap(map);
+
+  // Build the list of restaurants
+  lunchSpots.forEach(function(lunchSpotItem) {
+      self.lunchSpotList.push( new LunchSpot(lunchSpotItem) );
+  });
+
+  //Set up the function to filter the list based on user input  
+  this.filteredLunchSpotList = ko.computed( function() {
+		var filter = self.searchTerm().toLowerCase();
+		if (!filter) {
+			self.lunchSpotList().forEach(function(lunchSpotItem){
+				lunchSpotItem.visible(true);
+			});
+			return self.lunchSpotList();
+		} else {
+			return ko.utils.arrayFilter(self.lunchSpotList(), function(lunchSpotItem) {
+				var string = lunchSpotItem.name.toLowerCase();
+				var result = (string.search(filter) >= 0);
+				lunchSpotItem.visible(result);
+				return result;
+			});
+		}
+	}, self);
+};
+
+var LunchSpot = function(data) {
+
+  var self = this;
+  
+  // Set all the properties as knockout observables
+  this.name = data.name;
+  this.lat = data.lat;
+  this.lng = data.lng;
+  this.address = data.address;
+  this.url = data.url;
+  this.menuUrl = data.menuUrl;
+  this.comments = data.comments;
+  this.menuString = data.menuString;
+  
+  this.visible = ko.observable(true);
+
+  //Get additional business info by calling foursquare API
+  var clientId = 'KLLNTY1ABC42WPEWQSBCAYHQZBCI1KXDK1CTRWMWC4NDFDQO';
+  var clientSecret = 'COPZSGGSJGCXLUMWLK35HU5P1EYMYJNV4ZSLAPLALTEP3UEU';
+
+  var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.lng + '&client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
+
+  $.getJSON(foursquareURL).done(function(data) {
+    var results = data.response.venues[0];
+    self.url = results.url;
+    self.address = results.location.formattedAddress[0];
+    //not all places on foursquare have a menu, so if not, I'm indicating that in the info window
+    if (typeof results.menu === 'undefined'){
+      console.log("no menu exists");
+      self.menuString = 'No menu exists';
+    } else {
+      self.menuUrl = results.menu.url;
+      //this code isn't pretty, but I'm creating the entire a tag/href string to be passed to the info window only if the menu exists
+      var menuStringA = '<a target = "_blank" href=';
+      var menuStringB = '>';
+      var menuStringC = '</a>';
+      var menuStringFull = menuStringA + self.menuUrl +  menuStringB + 'menu' + menuStringC;
+      self.menuString = menuStringFull; 
+    }
+    }).fail(function() {
+       console.log("Call to foursquare API failed");
+       self.url = "no data retrieved";
+       self.address = "no data retrieved";
+       self.menuUrl = "no data retrieved";
+    });
+
+  this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
+
+  //set the marker which will appear on the map
+  this.marker = new google.maps.Marker({
+      position: new google.maps.LatLng(data.lat, data.lng),
+      map: map,
+      title: data.name
+  });
+
+  this.marker.addListener('click', function(){
+    self.contentString = '<div><div class="info-window-title"><b>' + data.name + '</b></div>' + '</br>' +
+      '<div class="4S-Title">' + 'Info from foursquare.com' + '</div>' + '</br>' + 
+      '<div class="4S-Data"><ul><li class="4S-Data">' + 'address: ' + self.address + '</li>' + '</br>' +
+      '<li class="4S-Data">' + self.menuString + '</li>' + '</br>' +
+      '<li class="4S-Data">' + 'URL: ' + '<a href="' + self.url + '" target="_blank">' + self.url + '</a></li></ul></div>' + '</br>' +
+      '<div class="content-comments">' + 'Local comments: ' + self.comments + '</div></div>';
+  
+    self.infoWindow.setContent(self.contentString);
+
+    self.infoWindow.open(map, this);
+
+    //animate the marker when they click on it
+    self.marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+        self.marker.setAnimation(null);
+    }, 2100);
+
+    //if they click somewhere else on the map, close the info window
+    map.addListener("click", function(){
+      self.infoWindow.close();
+    });
+  });
+  
+  //this function is bound to the html when they click on an item in the list
+  this.bounce = function(place) {
+    google.maps.event.trigger(self.marker, 'click');
+  };
+};
+
+// Kick everything off!
+function launchApp() {
+  console.log("launching app");
+	ko.applyBindings(new ViewModel());
+}
---
+++ js/lunchRecommendations.js
@@ -0,0 +1,63 @@
+//Define the lunch locations
+var lunchSpots = [
+    {
+		  name : 'Bagel Street Cafe',
+      lat : 37.762684,
+      lng : -121.962823,
+      comments: 'Good breakfast options! - Emmy B.'
+	  },
+    {
+      name : 'The Hopyard Ale House',
+      lat : 37.764023,
+      lng : -121.950976,
+		  comments: 'Good burgers, good beer selection for after work too. - John V.'
+    },
+    {
+      name : 'Dos Coyotes',
+      lat : 37.776935,
+      lng : -121.970229,
+		  comments: 'Use their online ordering option, makes it much easier. - Dan B.'
+    },
+    {
+      name : 'Panera Bread',
+      lat : 37.778880,
+      lng : -121.969713,
+		  comments: 'Get the grilled cheese sandwich with bacon, it\'s the best thing ever - Jack B.'
+    },
+    {
+      name: 'Buffalo Wild Wings',
+      lat : 37.764550,
+      lng : -121.952470,
+		  comments: 'Good wings, lots of TVs. Good for happy hour with a large group. - Carolyn G.'
+    },
+    {
+      name : 'Jimmy Johns Sandwiches',
+      lat : 37.763026,
+      lng : -121.952272,
+		  comments: 'Basic sandwiches, but well made and very quick service. - Stan W.'
+    },
+    {
+      name : 'Baja Fresh',
+      lat : 37.761653,
+      lng : -121.961658,
+		  comments: 'Decent option, at least it\'s close by, but it\'s overpriced. - Jeannine S.'
+    },
+    {
+      name : 'Fresh Millions',
+      lat : 37.772954,
+      lng : -121.973980,
+		  comments: 'Quite a few healthier selections than most places. - Mike P.'
+    },
+    {   
+      name : 'In-N-Out Burger',
+      lat : 37.779141,
+      lng : -121.977038,
+		  comments: 'Have to try it if this is your first time in California. - Scott F.'
+    },
+    {   
+      name : 'Zachary\'s Pizza',
+      lat : 37.777801,
+      lng : -121.972507,
+      comments: 'Chicago deep dish pizza. Can take a long time to prepare, make sure you have time. - Mike W.'
+    }
+];
